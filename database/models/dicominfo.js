d92eabbf5675abcc057f70c4534a4b81dd617d48
JeongHoon Lim <42137761+ljh7857@users.noreply.github.com>
2021-03-22 22:29:48 +0900

    update model

--- database/models/dicominfo.js
+++ database/models/dicominfo.js
@@ -1,22 +1,24 @@
 const mongoose = require('mongoose');
 
 const dInfoSchema = new mongoose.Schema({
-	StudyDate: {type : Date},
-	ModalityMG: {type: String},
-	InstitutionName: {type: String},
-	StudyDescription: {type: String},
-	PatientName: {type: String},
-	PatientID: {type: String},
-	PatientBirthDate: {type : Date},
-	PatientSex: {type: String},	
-	ReadingStatus: {type: String},
-	DoctorName: {type: String},
-	ReadingResult: {type: String},
-	RequestDate: {type : Date, default: Date.now},
-	ReadingDate: {type : Date},
-	Request: {type: String},
-	DICOMUrl: {type: String},
-	NumOfImg: {type: Number}
+	//IndexId: {type: Number},	//순번
+	StudyDate: {type : Date},	//촬영일자
+	ModalityMG: {type: String},	//검사장비구분
+	InstitutionName: {type: String},//촬영병원명
+	StudyDescription: {type: String},//검사정보
+	PatientName: {type: String},	//환자명
+	PatientID: {type: String},	//차트번호
+	PatientBirthDate: {type : Date},//생년월일
+	PatientSex: {type: String},	//성별
+	ReadingStatus: {type: String},//판독상태
+	DoctorName: {type: String},	//판독의사명
+	ReadingResult: {type: String},//판독결과
+	RequestDate: {type : Date, default: Date.now},//의뢰일자
+	ReadingDate: {type : Date},	//판독일자
+	Request: {type: String},	//요구사항
+	DICOMUrl: {type: String},	//dcm경로
+	NumOfImg: {type: Number},	//dcm이미지 수
+	//ReadingText: {type: String}	//상세 판독 결과
 });
-
+//dInfoSchema.index({IndexId:1});
 module.exports = mongoose.model('DICOMInfo',dInfoSchema);
--- database/models/user.js
+++ database/models/user.js
@@ -1,9 +1,11 @@
 const mongoose = require('mongoose');
 
 const userSchema = new mongoose.Schema({
-	Uid: {type: String},
+	Email: {type: String},
+	PassWord: {type: String},
 	DoctorName: {type: String},
-	AffiliatedHospital: {type: String}
+	Role: {type: String},	//요청자, 판독자
+	AffiliatedHospital: {type: String}	//소속 병원
 });
-userSchema.index({Uid: 1});
+userSchema.index({Email: 1});
 module.exports = mongoose.model('UserData',userSchema);
