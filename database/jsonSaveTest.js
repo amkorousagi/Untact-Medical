08ed198628bd1adf4bce018e770e86a6a9cb1844
JeongHoon Lim <42137761+ljh7857@users.noreply.github.com>
2021-03-30 23:00:12 +0900

    preprocessing age data (demo)

--- database/jsonSaveTest.js
+++ database/jsonSaveTest.js
@@ -11,6 +11,11 @@ console.log('data1');
 const fs = require('fs');
 var data = fs.readFileSync('./qwer.json');	//json연결 (나중엔 동적으로 받아야함)
 let a = JSON.parse(data);	//객체로 파싱
+if (a.PatientAge == 'No data'){
+	if ((a.PatientBirthDate!='No data')&&(a.StudyData!='No Data')){
+		a.PatientAge = a.StudyData.getFullYear() - a.PatientBirthDate.getFullYear()+1
+	}
+}
 var di = new dinfo();		//모델 인스턴스 생성
 Object.assign(di,a);		//모델 인스턴스에 파싱된Json 할당
 
